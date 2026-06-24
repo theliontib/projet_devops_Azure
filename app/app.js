6f4c1dc9d05de7bfe4ca140336001709cd132669
Thibaut <thibaut-huet@hotmail.fr>
2026-06-24 17:48:35 +0200

    add serveur code

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.end('Hello DevOps depuis Azure 🚀');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
