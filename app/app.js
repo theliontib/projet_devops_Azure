73337629ce28ddd7d3a87065f6064da0c6460503
Thibaut <thibaut-huet@hotmail.fr>
2026-06-30 16:43:45 +0200

    rajouté dans l'app.js l'ecoute sur 0.0.0.0 pour qu'il entende sur azure

--- app/app.js
+++ app/app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello DevOps depuis Azure 🚀');
 });
 
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0" () => {
   console.log('Server running on port 3000');
 });
