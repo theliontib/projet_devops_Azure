494e6ed94ecd21cbf278901ffe34fd85ddde8a49
Thibaut <thibaut-huet@hotmail.fr>
2026-07-01 16:31:10 +0200

    relance parceque j'avais pas bien identifié sur azure

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello DevOps depuis Azure et avec CI/CD !;
+  res.end('Hello DevOps depuis Azure et avec CI/CD et bien identifié !;
 });
 
 server.listen(3000, "0.0.0.0" () => {
