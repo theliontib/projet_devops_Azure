99ddebd0c8981f2d302e81a4a828a8f5794731b5
Thibaut <thibaut-huet@hotmail.fr>
2026-07-08 16:44:54 +0200

    ecriture du main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,17 @@
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-devops-tp"
+  location = "westeurope"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "registrydevopstp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
