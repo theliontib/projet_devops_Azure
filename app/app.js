3d36445b448cdfab0185b5cea467a412941112b7
theliontib <thibaut-huet@hotmail.fr>
2026-07-21 14:51:26 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello DevOps depuis Azure et avec CI/CD et bien identifié ! v1002');
 });
 
-server.listen(3000, "0.0.0.0" () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log('Server running on port 3000');
 });
